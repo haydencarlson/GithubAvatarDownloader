4c8a97d295accf0e3dc534f06db78222e64b2042
Hayden Carlson <haydog2@hotmail.com>
2016-10-11 16:03:05 +0000

    Update

--- download_avatars.js
+++ download_avatars.js
@@ -4,38 +4,49 @@ var fs = require('fs');
 var repoO = process.argv[2];
 var repoN = process.argv[3];
 
-var searchOptions = {
-  url: "https://api.github.com/repos/" + repoO + "/" + repoN + "/contributors",
-  path: "/",
-  headers: {
-    'User-Agent': 'heydoon'
-  },
-  auth: {
-    user: process.env.USERNAME,
-    pass: process.env.CLIENT_KEY
+
+var createRequestHandler = function(url) {
+  return {
+    url: url,
+    path: "/",
+    headers: {
+      'User-Agent': 'heydoon'
+    },
+    auth: {
+      user: process.env.USERNAME,
+      pass: process.env.CLIENT_KEY
+    }
   }
 }
 
-request(searchOptions, function(error, response, body) {
-  if (error) {
-    console.log("Error occurred:" +  " " + error);
-  } else {
-    console.log(response.statusCode);
 
-    var data = JSON.parse(body);
-    for (var i = 0; i < data.length; i++) {
-      downloadImageByURL(data[i].avatar_url, "./avatars/" + data[i].login);
+function getRepoContributors(repoOwner, repoName) {
+
+  var repoURL = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+
+  request(createRequestHandler(repoURL), function(error, response, body) {
+    if (error) {
+      console.log("Error occurred:" +  " " + error);
+    } else {
+      console.log(response.statusCode);
+
+      var data = JSON.parse(body);
+      for (var i = 0; i < data.length; i++) {
+        downloadImageByURL(data[i].avatar_url, "./avatars/" + data[i].login + "");
+      }
     }
-  }
-});
+  });
+}
+
+
 
 function downloadImageByURL (url, filePath) {
 
   request(url, {encoding: 'binary'}, function(error, response, body) {
-    var getExtension = response.headers['content-type'].split('/')[1];
-    fs.writeFile(filePath + "." + getExtension, body, 'binary', (err) => {
+    fs.writeFile(filePath, body, 'binary', (err) => {
       if (err) throw err;
       console.log("Saved to:" + filePath);
     });
   });
 }
+getRepoContributors(repoO, repoN);
